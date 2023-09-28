044d3fcc0383b04e793e29e36ff5ab7e59e453d2
SIKU-KR <peter012677@naver.com>
2023-09-28 19:59:51 +0900

    added queue.h

---
+++ source/queue.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<vector>
+#include<map>
+#include<tuple>
+
+using namespace std;
+
+struct Queue_Struct {
+	int orderNum;
+	map<int, unsigned int> orderBoxMap;
+	// 여기에 필요한 자료형 더하기
+};
