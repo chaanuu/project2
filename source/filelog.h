b215be22a8daeef34f380ecce8da6074d9311735
redredjoa <82462580+redredjoa@users.noreply.github.com>
2023-10-09 16:03:06 +0900

    Update filelog.h
    '주문데이터 받아오기' 수정

--- source/filelog.h
+++ source/filelog.h
@@ -52,10 +52,10 @@ int filelog(OrderInfo orderinfo) {
         string orderdata;
         int totalPrice =0;
         for (auto iter = orderinfo.information.begin(); iter != orderinfo.information.end(); iter++) {
-            orderdata += "," + getMenuName(iter->first) + "," + to_string(iter->second);
+            orderdata += "," + getMenuName(iter->first) + "," + to_string(iter->second) + "," + to_string(getMenuPrice(iter->first));
             totalPrice += getMenuPrice(iter->first) * iter->second;
         }
-        outputFile << date << "," << time << "," << orderinfo.number << "," << to_string(totalPrice) << orderdata << "\n";
+        outputFile << orderinfo.number << "," << date << "," << time  <<"," << to_string(totalPrice) << orderdata << "\n";
 
 
         
