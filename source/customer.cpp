9c19ca864e44b702fcd15d05d6c9724d1fb52f13
SIKU-KR <peter012677@naver.com>
2023-11-30 13:33:53 +0900

    .

---
+++ source/customer.cpp
@@ -0,0 +1,193 @@
+#include"customer.h"
+
+using namespace std;
+
+
+class DB {
+private:
+    sqlite3* db;
+    std::string dbPath;
+    time_t now = time(nullptr);
+
+    static int callback(void* data, int argc, char** argv, char** azColName) {
+        auto* records = static_cast<vector<orderEntry>*>(data);
+        records->emplace_back(atoi(argv[0]), argv[1], atoi(argv[2]), argv[3]);
+        return 0;
+    }
+
+    static int callback2(void* data, int argc, char** argv, char** azColName) {
+        auto* records = static_cast<vector<couponEntry>*>(data);
+        records->emplace_back(atoi(argv[0]), argv[1], argv[2]);
+        return 0;
+    }
+
+    static int callback3(void* data, int argc, char** argv, char** azColName) {
+        auto* record = static_cast<couponEntry*>(data);
+        // 첫 번째 행만 처리하고 이후 호출은 무시
+        record->id = atoi(argv[0]);
+        record->hp = argv[1];
+        record->exp = argv[2];
+        return 0;
+    }
+
+    string yyyymmdd() {
+        time_t now = time(nullptr);
+        tm* ltm = localtime(&now);
+        stringstream ss;
+        ss << 1900 + ltm->tm_year;
+        ss << std::setw(2) << std::setfill('0') << 1 + ltm->tm_mon;
+        ss << std::setw(2) << std::setfill('0') << ltm->tm_mday;
+        return ss.str();
+    }
+
+public:
+    DB(const std::string& dbPath) : dbPath(dbPath), db(nullptr) {
+        if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
+            cerr << "Can't open database: " << sqlite3_errmsg(db) << endl;
+            exit(0);
+        }
+    }
+
+    ~DB() {
+        if (db) {
+            sqlite3_close(db);
+        }
+    }
+
+    vector<orderEntry> getRecordsByHp(const string hpValue) {
+        vector<orderEntry> records;
+        string sql = "SELECT * FROM orderLog WHERE hp = '" + hpValue + "'";
+        char* errMsg = nullptr;
+        if (sqlite3_exec(db, sql.c_str(), callback, &records, &errMsg) !=
+            SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+        return records;
+    }
+
+    unsigned int makeCoupon(const string hpValue) {
+        unsigned int coupon_count = 0;
+        unsigned int total = 0;
+
+        vector<orderEntry> record = getRecordsByHp(hpValue);
+        vector<orderEntry>::iterator iter;
+        vector<orderEntry> deleteEntries;
+        struct orderEntry editEntry;
+
+        string sql;
+        string date = yyyymmdd();
+        char* errMsg = nullptr;
+
+        // 현재 주문 총액 구하기
+        for (iter = record.begin(); iter != record.end(); iter++) {
+            total += iter->amount;
+        }
+
+        // 쿠폰 추가하기
+        coupon_count = total / 10000;
+        sql = "INSERT INTO couponBook (hp, exp) VALUES (" + hpValue + ", " + date +
+            ")";
+        for (int i = 0; i < coupon_count; i++) {
+            if (sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg) != SQLITE_OK) {
+                cerr << "SQL error: " << errMsg << endl;
+                sqlite3_free(errMsg);
+            }
+        }
+
+        // 삭제할 Entry와 수정할 Entry를 찾기
+        for (iter = record.begin(); iter != record.end(); iter++) {
+            if ((total - iter->amount) < 0) {
+                editEntry = *iter;
+                editEntry.amount -= total;
+                break;
+            }
+            deleteEntries.emplace_back(*iter);
+            total -= iter->amount;
+        }
+
+        // 삭제할 Entry 삭제하기
+        for (iter = deleteEntries.begin(); iter != deleteEntries.end(); iter++) {
+            sql = "DELETE FROM orderLog WHERE id = " + to_string(iter->id);
+            if (sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg) != SQLITE_OK) {
+                cerr << "SQL error: " << errMsg << endl;
+                sqlite3_free(errMsg);
+            }
+        }
+
+        // 수정할 Entry 수정하기
+        sql = "UPDATE orderLog SET amount " + to_string(editEntry.amount) +
+            " WHERE id = " + to_string(editEntry.id);
+
+        if (sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg) != SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+
+        return coupon_count;
+    }
+
+    vector<couponEntry> getCouponsAvailable(const string hpValue) {
+        vector<couponEntry> coupons;
+        string sql = "SELECT * FROM couponBook WHERE hp = '" + hpValue + "'";
+        char* errMsg = nullptr;
+        if (sqlite3_exec(db, sql.c_str(), callback2, &coupons, &errMsg) !=
+            SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+        return coupons;
+    }
+
+    void useCoupon(const string hpValue) {
+        struct couponEntry used_coupon;
+
+        // 해당 전화번호에 대한 가장 작은 ID를 찾습니다.
+        string sql = "SELECT MIN(id) FROM couponBook WHERE hp = '" + hpValue + "'";
+        char* errMsg = nullptr;
+        if (sqlite3_exec(db, sql.c_str(), callback3, &used_coupon, &errMsg) !=
+            SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+
+        // 찾은 쿠폰을 삭제합니다.
+        sql = "DELETE FROM couponBook WHERE id = " + to_string(used_coupon.id);
+        if (sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg) != SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+    }
+
+    void checkDB() {
+        string currentDate = yyyymmdd();  // 현재 날짜 가져오기
+
+        // 7일 전 날짜 계산
+        tm ltm = {};
+        strptime(currentDate.c_str(), "%Y%m%d", &ltm);  // 문자열을 tm 구조체로 변환
+        ltm.tm_mday -= 7;
+        mktime(&ltm);
+
+        stringstream ss;
+        ss << 1900 + ltm.tm_year;
+        ss << std::setw(2) << std::setfill('0') << 1 + ltm.tm_mon;
+        ss << std::setw(2) << std::setfill('0') << ltm.tm_mday;
+        string sevenDaysAgoDate = ss.str();  // 7일 전 날짜 문자열로 변환
+
+        // orderLog 테이블에서 7일보다 이전인 기록 삭제
+        string sql =
+            "DELETE FROM orderLog WHERE date <= '" + sevenDaysAgoDate + "'";
+        char* errMsg = nullptr;
+        if (sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg) != SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+
+        // couponBook 테이블에서 7일보다 이전인 기록 삭제
+        sql = "DELETE FROM couponBook WHERE exp <= '" + sevenDaysAgoDate + "'";
+        if (sqlite3_exec(db, sql.c_str(), 0, 0, &errMsg) != SQLITE_OK) {
+            cerr << "SQL error: " << errMsg << endl;
+            sqlite3_free(errMsg);
+        }
+    }
+};
--- source/customer.h
+++ source/customer.h
@@ -1,157 +1,54 @@
+#pragma once
+#include <ctime>
+#include <iomanip>
 #include <iostream>
+#include <map>
+#include <sstream>
 #include <string>
+#include <vector>
+
 #include "sqlite3.h"
 
-class MembershipDB {
+struct orderEntry {
+    int id;
+    string hp;
+    int amount;
+    string date;
+
+    orderEntry() : id(0), hp(""), amount(0), date("") {}
+
+    orderEntry(int id, string hp, int amount, string date)
+        : id(id), hp(hp), amount(amount), date(date) {}
+};
+
+struct couponEntry {
+    int id;
+    string hp;
+    string exp;
+
+    couponEntry() : id(0), hp(""), exp("") {}
+
+    couponEntry(int id, string hp, string exp) : id(id), hp(hp), exp(exp) {}
+};
+
+class DB {
 private:
     sqlite3* db;
-    std::string dbPath;
+    string dbPath;
 
-    bool executeSQL(const std::string& sql) {
-        char* errMsg = nullptr;
-        int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
-        if (rc != SQLITE_OK) {
-            std::cerr << "SQL error: " << errMsg << std::endl;
-            sqlite3_free(errMsg);
-            return false;
-        }
-        return true;
-    }
+    static int callback(void* data, int argc, char** argv, char** azColName);
+    static int callback2(void* data, int argc, char** argv, char** azColName);
+    static int callback3(void* data, int argc, char** argv, char** azColName);
+
+    string yyyymmdd();
 
 public:
-    MembershipDB(const std::string& dbPath) : dbPath(dbPath), db(nullptr) {}
+    DB(const string& dbPath);
+    ~DB();
 
-    ~MembershipDB() {
-        if (db) {
-            sqlite3_close(db);
-        }
-    }
-
-    bool open() {
-        if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
-            std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
-            return false;
-        }
-
-        std::string createTableSQL = R"(
-            CREATE TABLE IF NOT EXISTS Membership (
-                PhoneNumber TEXT PRIMARY KEY,
-                TotalAmount INTEGER NOT NULL,
-                CouponsUsed INTEGER DEFAULT 0
-            );
-        )";
-
-        return executeSQL(createTableSQL);
-    }
-
-    bool executeOrder(const std::string& phoneNumber, int orderAmount) {
-        sqlite3_stmt* stmt;
-        std::string sql = "SELECT TotalAmount FROM Membership WHERE PhoneNumber = ?";
-
-        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-            return false;
-        }
-
-        sqlite3_bind_text(stmt, 1, phoneNumber.c_str(), -1, SQLITE_STATIC);
-
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            // Customer exists
-            int currentAmount = sqlite3_column_int(stmt, 0);
-            sqlite3_finalize(stmt);
-
-            sql = "UPDATE Membership SET TotalAmount = TotalAmount + ? WHERE PhoneNumber = ?";
-            if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-                return false;
-            }
-            sqlite3_bind_int(stmt, 1, orderAmount);
-            sqlite3_bind_text(stmt, 2, phoneNumber.c_str(), -1, SQLITE_STATIC);
-        }
-        else {
-            // New customer
-            sqlite3_finalize(stmt);
-
-            sql = "INSERT INTO Membership (PhoneNumber, TotalAmount) VALUES (?, ?)";
-            if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-                return false;
-            }
-            sqlite3_bind_text(stmt, 1, phoneNumber.c_str(), -1, SQLITE_STATIC);
-            sqlite3_bind_int(stmt, 2, orderAmount);
-        }
-
-        if (sqlite3_step(stmt) != SQLITE_DONE) {
-            sqlite3_finalize(stmt);
-            return false;
-        }
-
-        sqlite3_finalize(stmt);
-        return true;
-    }
-
-    int getCouponsAvailable(const std::string& phoneNumber) {
-        sqlite3_stmt* stmt;
-        std::string sql = "SELECT TotalAmount, CouponsUsed FROM Membership WHERE PhoneNumber = ?";
-
-        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-            return 0;
-        }
-
-        sqlite3_bind_text(stmt, 1, phoneNumber.c_str(), -1, SQLITE_STATIC);
-
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            int totalAmount = sqlite3_column_int(stmt, 0);
-            int couponsUsed = sqlite3_column_int(stmt, 1);
-            sqlite3_finalize(stmt);
-
-            int couponsEarned = totalAmount / 20000;
-            return couponsEarned - couponsUsed;
-        }
-
-        sqlite3_finalize(stmt);
-        return 0;
-    }
-
-    bool useCoupon(const std::string& phoneNumber) {
-        sqlite3_stmt* stmt;
-        std::string sql = "SELECT CouponsUsed FROM Membership WHERE PhoneNumber = ?";
-
-        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-            return false;
-        }
-
-        sqlite3_bind_text(stmt, 1, phoneNumber.c_str(), -1, SQLITE_STATIC);
-
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            // Customer exists
-            int couponsUsed = sqlite3_column_int(stmt, 0);
-            sqlite3_finalize(stmt);
-
-            // Check if there are available coupons to use
-            if (getCouponsAvailable(phoneNumber) > 0) {
-                sql = "UPDATE Membership SET CouponsUsed = CouponsUsed + 1 WHERE PhoneNumber = ?";
-                if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-                    return false;
-                }
-                sqlite3_bind_text(stmt, 1, phoneNumber.c_str(), -1, SQLITE_STATIC);
-            }
-            else {
-                // No available coupons to use
-                sqlite3_finalize(stmt);
-                return false;
-            }
-        }
-        else {
-            // No such customer exists
-            sqlite3_finalize(stmt);
-            return false;
-        }
-
-        if (sqlite3_step(stmt) != SQLITE_DONE) {
-            sqlite3_finalize(stmt);
-            return false;
-        }
-
-        sqlite3_finalize(stmt);
-        return true;
-    }
-
+    vector<orderEntry> getRecordsByHp(const string hpValue);
+    unsigned int makeCoupon(const string hpValue);
+    vector<couponEntry> getCouponsAvailable(const string hpValue);
+    void useCoupon(const string hpValue);
+    void checkDB();
 };
